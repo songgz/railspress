504453871e7f58fd392cf69c5f8b6e98249acb60
shunwitter <polytechnics.shun@gmail.com>
2013-11-20 21:03:59 +0100

    * change ad position * change code css

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
 
 	def index
 		if params[:tag]
-			@posts = Post.tagged_with(params[:tag]).where("published=?", true).paginate(page: params[:page], per_page: 10, total_entries: 80)
+			@posts = Post.tagged_with(params[:tag]).where("published=?", true).paginate(page: params[:page], per_page: 10)
 		else
 			@posts = Post.where("published=?", true).paginate(page: params[:page], per_page: 10)
 		end
